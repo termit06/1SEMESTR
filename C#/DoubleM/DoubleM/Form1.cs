4e81f9cf29748183c5d596e9b348df44d147952b
Sk1L3D <146962543+termit06@users.noreply.github.com>
2024-12-29 13:16:11 +0300

    Double Massive
    Вариант 7

---
+++ C#/DoubleM/DoubleM/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoubleM
+{
+    public partial class Form1 : Form
+    {
+
+        Test Op = new Test();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Op.N = Convert.ToInt32(textBox1.Text);
+            Op.M = Convert.ToInt32(textBox2.Text);
+
+            Op.A = Op.InpMatrix(listBox1);
+
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Op.ChildrenPlayReplays();
+            Op.OutMatrix(Op.X, listBox2);
+        }
+    }
+}
---
+++ C#/DoubleM/DoubleM/Operations.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// подключение элементов формы
+using System.Windows.Forms;
+
+namespace DoubleM
+{
+    public class Operations
+    {
+
+        private double[,] a;
+        private double[,] x;
+        private int n;
+        private int m;
+
+        // количество строк и столбцов n*m
+        public int N
+        {
+            get { return n; }
+            set { n = value; }
+        }
+        // количество строк и столбцов (private)
+        
+        public int M
+        {
+            get { return m; }
+            set { m = value; }
+        }
+        
+        public double[,] A
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        
+        public double[,] X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private void SwitchRows(int[,] array)
+        {
+            var iMax = array.GetLength(0);
+            var jMax = array.GetLength(1);
+            for (int i = 0; i < iMax; i = i + 2)
+            {
+                for (int j = 0; j < jMax; j++)
+                {
+                    var a = array[i, j];
+                    array[i, j] = array[i + 1, j];
+                    array[i + 1, j] = a;
+                }
+            }
+        }
+        private void Replays()
+        {
+            x = new double[n, m];
+
+            for(int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    x[i, m - 1 - j] = a[i, j];
+                }
+            }
+
+        }
+
+        protected void PlayuReplays()
+        {
+            Replays();
+        }
+    }
+
+    public class Test : Operations
+    {
+        Random rand = new Random();
+
+        public double[,] InpMatrix(ListBox lb)
+        {
+            double[,] array = new double[N, M];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    array[i, j] = Math.Round(rand.NextDouble() * 100, 2);
+                }
+            }
+
+            OutMatrix(array, lb);
+
+            return array;
+
+        }
+        // Метод вывода двумерной матрицы
+        public void OutMatrix(double[,] array, ListBox lb)
+        {
+            lb.Items.Clear();
+            for (int i = 0; i < N; i++)
+            {
+                string s = "";
+                for (int j = 0; j < M; j++)
+                {
+                    s += array[i, j].ToString() + "  ";
+                }
+                lb.Items.Add(s);
+            }
+
+        }
+
+        public void ChildrenPlayReplays()
+        {
+            PlayuReplays();
+        }
+    }
+}
