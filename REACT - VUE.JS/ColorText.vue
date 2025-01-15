c6734bd826e2ef9587a4ece3c597a56c475145de
Sk1L3D <146962543+termit06@users.noreply.github.com>
2025-01-15 13:46:49 +0300

    Add files via upload

---
+++ REACT - VUE.JS/ColorText.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="title" :style="[`color: ${colortext}`]">
+{{mainText}}
+</div>
+<div class="title2" :style="[`color: ${colortext}`]">
+{{mainText}}
+<div class="title3" :style="[`color: ${colortext}`]">
+{{mainText}}
+</div>
+</div>
+</template>
+
+<script setup>
+defineProps({
+    mainText:{
+        type: String,
+        required: true
+    },
+    colorText:{
+        type: Number,
+        required: true
+    }
+    
+})
+</script>
+
+<style lang="scss" scoped>
+.title{
+    font-size: 100px;
+    color: rgb(30, 255, 0);
+}
+.title2{
+    font-size: 100px;
+    color: rgb(238, 255, 0);
+}
+.title3{
+    font-size: 100px;
+    color: red;
+}
+</style>
---
+++ REACT - VUE.JS/ColorView.vue
@@ -0,0 +1,13 @@
+<template>
+<span>
+<ColorText mainText="GREEN YELLOW RED" colorText="any"/> 
+</span>
+</template>
+
+<script setup>
+import ColorText from '@/components/ColorText.vue';
+</script>
+
+<style scoped>
+
+</style>
